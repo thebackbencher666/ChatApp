e1c13f8900ef3ee66b3c50fef2e2c1102383bfe5
arif rabbani_49 <rabbaniarif697@gmail.com>
2025-04-25 16:03:25 +0600

    Firebase initialized with email/password authentication

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './screens/WelcomeScreen';
-
+import SignInScreen from './screens/SignInScreen';
+import SignUpScreen from './screens/SignUpScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -14,6 +15,8 @@ export default function App() {
           component={WelcomeScreen}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDN8I1WsN9ijyk0KQv-9F8ANPMErSsh8KY",
+  authDomain: "chatapp-97a9f.firebaseapp.com",
+  projectId: "chatapp-97a9f",
+  storageBucket: "chatapp-97a9f.firebasestorage.app",
+  messagingSenderId: "798267306891",
+  appId: "1:798267306891:web:db648bf708a5814bd3358e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
