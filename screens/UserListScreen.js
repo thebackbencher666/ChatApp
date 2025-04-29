a7f59d1acd6997e7e31689e59c31bbd35b063369
arif rabbani_49 <rabbaniarif697@gmail.com>
2025-04-29 18:29:29 +0600

    added some buttons in userlistscreen

--- screens/UserListScreen.js
+++ screens/UserListScreen.js
@@ -1,22 +1,20 @@
-// UsersListScreen.js
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import { collection, getDocs } from 'firebase/firestore';
 import { auth, db } from '../firebase';
-import { useNavigation } from '@react-navigation/native';
+import { signOut } from 'firebase/auth';
 
-const UsersListScreen = ({navigation}) => {
+const UsersListScreen = ({ navigation }) => {
   const [users, setUsers] = useState([]);
-  const navigation = useNavigation();
 
   useEffect(() => {
     const fetchUsers = async () => {
-        const usersCollection = collection(db, 'users');
-        const userSnapshot = await getDocs(usersCollection);
-        const userList = userSnapshot.docs
-            .map((doc) => ({ id: doc.id, ...doc.data() }))
-            .filter((user) => user.email !== auth.currentUser.email);
-        setUsers(userList);
+      const usersCollection = collection(db, 'users');
+      const userSnapshot = await getDocs(usersCollection);
+      const userList = userSnapshot.docs
+        .map((doc) => ({ id: doc.id, ...doc.data() }))
+        .filter((user) => user.email !== auth.currentUser.email);
+      setUsers(userList);
     };
 
     fetchUsers();
@@ -26,27 +24,25 @@ const UsersListScreen = ({navigation}) => {
     navigation.navigate('ChatScreen', { user });
   };
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.userItem} onPress={() => handleUserPress(item)}>
-      <Text style={styles.username}>{item.email}</Text>
-    </TouchableOpacity>
-  );
+  const handleLogout = () => {
+    signOut(auth).then(() => navigation.replace('SignIn'));
+  };
 
   return (
     <View style={styles.container}>
-      <FlatList 
+      <FlatList
         data={users}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-            <TouchableOpacity style = {styles.userItem} onPress={() => handleUserPress(item)}>
-                <text>{item.email}</text>
-            </TouchableOpacity>
+          <TouchableOpacity style={styles.userItem} onPress={() => handleUserPress(item)}>
+            <Text style={styles.username}>{item.email}</Text>
+          </TouchableOpacity>
         )}
       />
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
